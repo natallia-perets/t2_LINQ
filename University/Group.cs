06a3850b26176f68ae356e542333b69d04fe4778
Natallia Perets <n.perets@intetics.com>
2014-07-18 17:14:43 +0300

    LINQ works

---
+++ University/Group.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    public enum GroupNames
+    {
+        BackEnd,
+        FrontEnd,
+        QA
+    };
+
+    public class Group
+    {
+        private List<MyLinkedList.Person> students;
+        private GroupNames groupName;
+
+        public Group(GroupNames name, List<MyLinkedList.Person> students)
+        {
+            groupName = name;
+            this.students = students;
+
+        }
+        public void Add(MyLinkedList.Person student)
+        {
+            students.Add(student);
+        }
+
+        public GroupNames GroupName
+        {
+            get { return groupName; }
+            set { groupName = value; }
+        }
+        public List<MyLinkedList.Person> Students
+        {
+            get { return students; }
+            set { students = value; }
+        }
+    }
+}
---
+++ University/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace University
+{
+    class Program
+    {
+        private static void Main(string[] args)
+        {
+            List<MyLinkedList.Person> l1 = new List<MyLinkedList.Person>();
+            l1.Add(new MyLinkedList.Person("Ann", "Ray", 20));
+            l1.Add(new MyLinkedList.Person("Cary", "I", 12));
+            l1.Add(new MyLinkedList.Person("Helen", "Carr", 19));
+            List<MyLinkedList.Person> l2 = new List<MyLinkedList.Person>();
+            l2.Add(new MyLinkedList.Person("John", "Doz", 20));
+            l2.Add(new MyLinkedList.Person("Ray", "Smith", 19));
+            l2.Add(new MyLinkedList.Person("Viki", "Ol", 18));
+            Group gr1 = new Group(GroupNames.BackEnd, l1);
+            Group gr2 = new Group(GroupNames.FrontEnd, l2);
+            List<Group> glist = new List<Group>();
+            glist.Add(gr1);
+            glist.Add(gr2);
+            var queryresult = from p in glist select p.Students.Average(s => s.Age);
+            foreach (int d in queryresult)
+            {
+                Console.WriteLine(d.ToString());
+            }
+            Console.ReadLine();
+        }
+            
+    }
+}
